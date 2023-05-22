cb4fa7e9565f8c4bec7fd911c162e839948ec377
klozano <klozano17@gmail.com>
2023-05-21 21:35:36 -0400

    added code to isntall and database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const getDb = await openDB("jate", 1);
+    const text = getDb.transaction("jate", "readnwrite");
+    const store = text.objectStore("jate");
+    const req = store.put({ id: 1, value: content });
+    const res = await req;
+  } catch (err) {
+    console.error("putDb not implemented");
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const getDb = await openDB("jate", 1);
+    const text = getDb.transaction("jate", "read");
+    const store = text.objectStore("jate");
+    const req = store.getAll();
+    const res = await req;
+    return res?.value;
+  } catch (err) {
+    console.error("getDb not implemented");
+  }
+};
 
 initdb();
